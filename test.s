96348ab4feda9547603578559d97a3534bc39a58
qian160 <75466046+qian160@users.noreply.github.com>
2022-08-06 01:28:15 +0800

    Update test.s

--- test.s
+++ test.s
@@ -1,8 +1,10 @@
 _start:
-  li x1,0x11
-  li x2,0x22
+  li x1,0x11      #addi x1,x0,0x11
+  sb x1,0(x0)
+  lb x2,0(x0)
+  mv x3,x2        #read after load
   bge x2,x1,target
-  li x3,0x33
+  li x3,0x33      #should not be execuated
   li x4,0x44
 target:
   beqz x0,target

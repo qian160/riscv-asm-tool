b7dd40ba17a9a66bf02422464d083b6fe4560725
qian160 <75466046+qian160@users.noreply.github.com>
2022-08-14 14:57:42 +0800

    Update as.cpp

--- as.cpp
+++ as.cpp
@@ -2,18 +2,18 @@
 #include<fstream>
 using namespace std;
 #define LS line[start]
-//when meet a .org psedo op, fill the space with nop
+//the difficulty is to deal with the .org psedo op
 int64_t hex2dec(string hex){
-    	int64_t result = 0;		//res = res << 4 + hex[i]
-    	int64_t adder  = 0;
-    	for(char c: hex){
-		result = result << 4;
-		if(c == ' ') 			adder = 0;
-		else if(c >= '0' && c <= '9') 	adder = c - '0';
-		else 				adder = c - 'a' + 10;
-		result += adder;
-    	}
-    	return result;
+    int64_t result = 0;		//res = res << 4 + hex[i]
+    int64_t adder  = 0;
+    for(char c: hex){
+	result = result << 4;
+	if(c == ' ') adder = 0;
+	else if(c >= '0' && c <= '9') adder = c - '0';
+	else 	adder = c - 'a' + 10;
+	result += adder;
+    }
+    return result;
 }
 
 string getAddress(string line){
@@ -26,38 +26,43 @@ string getAddress(string line){
 }
 
 bool check(string s){
-	/*three cases:
-		1. label 2.  ... 3.  empty line
+	/*four bad cases:
+		1. label 2.  ... 3.  empty line 4. unimp
 	*/
 	int len = s.length();
-	if(len == 0 || s[len -1] == ':' || s[len - 1] == '.') return false;
+	if(s[len -1] == ':' || s[len - 1] == '.' || len == 0 || s.substr(len - 5) == "unimp") return false;
 	return true;
 }
 
 int main(int argc, char ** argv)
 {
+	//bug: when first line didn't pass the check due to an 'unimp', an insert is needed. But diff will be kept to 4 so no insert is performed
 	ifstream in;
 	in.open("asm.txt");
 	if(in.fail()){
-		cout<<"open file asm.txt failed!\n";
+		cout << "file asm.txt open failed!\n";
 		exit(1);
 	}
 	string line;
-	int n = 7;					//start at line 7 
-	while(n--)getline(in,line);
+	int n = 7;
+	while(n--)getline(in,line);		//start at line 7
 	int64_t oldAddr = -4;
-	int64_t newAddr = 0;				//good: the difference is 4(no .org)
+	int64_t newAddr = 0;			//good: the difference is 4(no .org)
 	while(getline(in,line))
 	{
-		if(!check(line)) continue;
+		if(!check(line)) {
+			oldAddr = oldAddr >= 0? oldAddr : 0;
+			continue;
+		}
+		//when meet a .org psedo op, fill the space with nop
 		newAddr = hex2dec(getAddress(line));
 		int64_t diff = newAddr - oldAddr;
-		if(diff > 4){				//need to insert nop first
+		if(diff > 4){			//need to insert nop first
 			for (int i = diff >> 2; i > 0 ; i--)
 				cout<<"00000013"<<endl;			
 		}
 		int start = 0;
-		while(LS != ':') start ++;
+		while(line[start] != ':') start ++;
 		while(!isdigit(LS) && !(LS >= 'a' && LS <= 'f')) start ++;
 		for(int i = start; i < start + 8; i++)
 			cout<<line[i];

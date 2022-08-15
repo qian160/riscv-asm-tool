2577c67e7b0232dfc09fb8952f2adb0f7d4e9486
qian160 <75466046+qian160@users.noreply.github.com>
2022-08-16 01:01:50 +0800

    Update as.cpp

--- as.cpp
+++ as.cpp
@@ -1,21 +1,19 @@
-//g++ -o as as.cpp
 #include<iostream>
 #include<fstream>
+#include<regex>
 using namespace std;
-#define LS line[start]
 #define LL line.length()
-
 int64_t hex2dec(string hex){
-    int64_t result = 0;		//res = res << 4 + hex[i]
-    int64_t adder  = 0;
-    for(char c: hex){
-	result = result << 4;
-	if(c == ' ') adder = 0;
-	else if(c >= '0' && c <= '9') adder = c - '0';
-	else 	adder = c - 'a' + 10;
-	result += adder;
-    }
-    return result;
+	int64_t result = 0;		//res = res << 4 + hex[i]
+	int64_t adder  = 0;
+	for(char c: hex){
+		result = result << 4;
+		if(c == ' ') 			adder = 0;
+		else if(c >= '0' && c <= '9') 	adder = c - '0';
+		else 				adder = c - 'a' + 10;
+		result += adder;
+    	}
+	return result;
 }
 
 string getAddress(string line){
@@ -28,10 +26,10 @@ string getAddress(string line){
 }
 
 bool check(string s/*, int64_t & oldAddr*/){
-	/*four cases:
+	/*three cases:
 		1. label 2.  ... 3.  empty line 4. unimp	*/
 	int len = s.length();
-	if(s[len -1] == ':' || s[len - 1] == '.' || len == 0 || s.substr(len - 5) == "unimp") return false;
+	if(len == 0 || s[len -1] == ':' || s[len - 1] == '.' || s.substr(len - 5) == "unimp") return false;
 	return true;
 }
 
@@ -47,23 +45,22 @@ int main(int argc, char ** argv)
 	int n = 7;
 	while(n--)getline(in,line);		//start at line 7
 	int64_t oldAddr = -4;
-	int64_t newAddr = 0;			//good: the difference is 4
-	bool lastIsUnimp = false;
+	int64_t newAddr = 0;			//good: the difference is 4(no .org)
 	while(getline(in,line))
 	{
 		if(!check(line))continue;
-		//when meet a .org psedo op, fill the space with nop
 		newAddr = hex2dec(getAddress(line));
 		int64_t diff = newAddr - oldAddr;
 		if(diff > 4){			//need to insert nop first
-			for (int i = diff >> 2   ; i > 1 ; i--)
+			for (int64_t i = diff >> 2 ; i > 1 ; i--)
 				cout<<"00000013"<<endl;			
 		}
-		int start = 0;
-		//find the start position of the machine code
-		while(line[start] != ':') start ++;
-		while(!isdigit(LS) && !(LS >= 'a' && LS <= 'f')) start ++;
-		cout << line.substr(start, start + 8) << endl;
+		regex inst("[0-9a-f]{8}");
+		smatch m;
+		auto ret = regex_search(line, m, inst, regex_constants::match_default);
+		for(auto &a : m)
+			cout<<a;
+		cout<<endl;
 		oldAddr = newAddr;
 	}
 	in.close();

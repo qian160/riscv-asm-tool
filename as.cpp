fba3e63a96fe6b006223d7298cee036bc1f99c19
qian160 <75466046+qian160@users.noreply.github.com>
2022-08-16 14:58:52 +0800

    Update as.cpp

--- as.cpp
+++ as.cpp
@@ -2,7 +2,7 @@
 #include<fstream>
 #include<regex>
 using namespace std;
-int64_t hex2dec(string hex)
+int64_t hex2dec(string & hex)
 {
 	int64_t result = 0;		//res = res << 4 + hex[i]
 	int64_t adder  = 0;
@@ -16,7 +16,7 @@ int64_t hex2dec(string hex)
 	return result;
 }
 
-string getAddress(string line)
+string getAddress(string & line)
 {
 	string s = "";
 	for(char c: line){
@@ -26,7 +26,7 @@ string getAddress(string line)
 	return s;
 }
 
-bool check(string s)
+bool check(string & s)
 {
 	/*	four bad cases:	1. label 2.  ... 3.  empty line 4. unimp	*/
 	int len = s.length();
@@ -39,7 +39,7 @@ int main(int argc, char ** argv)
 	ifstream in;
 	in.open("asm.txt");
 	if(in.fail()){
-		cout << "file asm.txt open failed!\n";
+		cout << "open file asm.txt failed!\n";
 		exit(1);
 	}
 	string line;
@@ -52,7 +52,8 @@ int main(int argc, char ** argv)
 		if(!check(line))continue;
 		newAddr = hex2dec(getAddress(line));
 		int64_t diff = newAddr - oldAddr;
-		if(diff > 4){			//need to insert nop first
+		if(diff > 4)
+		{				//need to insert nop first
 			for (int64_t i = diff >> 2 ; i > 1 ; i--)
 				cout<<"00000013"<<endl;			
 		}

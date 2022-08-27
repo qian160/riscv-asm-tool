0d2a6c99ca8d572fe55b7086792b014763fe5946
qian160 <75466046+qian160@users.noreply.github.com>
2022-08-28 00:13:26 +0800

    Update as.cpp

--- as.cpp
+++ as.cpp
@@ -4,8 +4,7 @@
 using namespace std;
 
 regex addr("[0-9a-f]{1,}");
-regex inst("[0-9a-f]{8}");
-smatch m;
+regex inst("\\s[0-9a-f]{8}\\s");
 
 int64_t hex2dec(string hex)
 {
@@ -13,7 +12,8 @@ int64_t hex2dec(string hex)
 	int64_t adder  = 0;
 	for(char c: hex){
 		result = result << 4;
-		if(c >= '0' && c <= '9') 	adder = c - '0';
+		if(c == ' ') 			adder = 0;
+		else if(c >= '0' && c <= '9') 	adder = c - '0';
 		else 				adder = c - 'a' + 10;
 		result += adder;
     	}
@@ -22,16 +22,9 @@ int64_t hex2dec(string hex)
 
 string getAddress(string line)
 {
+	smatch m;
 	regex_search(line, m, addr, regex_constants::match_default);
 	return m.str();
-}
-
-bool check(string & s)
-{
-	/*four bad cases:	1. label  2.  ... 3.  empty line  4. unimp	*/
-	int len = s.length();
-	if(len == 0 || s[len -1] == ':' || s[len - 1] == '.' || s.substr(len - 5) == "unimp") return false;
-	return true;
 }
 
 int main(int argc, char ** argv)
@@ -46,10 +39,12 @@ int main(int argc, char ** argv)
 	int n = 7;
 	while(n--)getline(in,line);		//start at line 7
 	int64_t oldAddr = -4;
-	int64_t newAddr = 0;			//good: the difference is 4
+	int64_t newAddr = 0;			//good: the difference is 4(no .org)
 	while(getline(in,line))
 	{
-		if(!check(line))continue;
+		smatch m;
+		regex_search(line, m, inst, regex_constants::match_default);
+		if(m.empty())continue;
 		newAddr = hex2dec(getAddress(line));
 		int64_t diff = newAddr - oldAddr;
 		if(diff > 4)
@@ -57,8 +52,7 @@ int main(int argc, char ** argv)
 			for (int64_t i = diff >> 2 ; i > 1 ; i--)
 				cout<<"00000013"<<endl;			
 		}
-		regex_search(line, m, inst, regex_constants::match_default);
-		cout << m.str() << endl;
+		cout << m.str().substr(1, 8) << endl;
 		oldAddr = newAddr;
 	}
 	in.close();
